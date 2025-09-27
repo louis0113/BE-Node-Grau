36081ad1346f2fcc23e042c9c65561f40cd0a3be
louis0113 <luizhenrique011304@gmail.com>
2025-09-27 20:56:48 -0300

    Adicionando generators functions

--- Objetos Iteraveis/iteraveis.js
+++ Objetos Iteraveis/iteraveis.js
@@ -1,19 +1,36 @@
 class Playlist {
 
-	constructor(musicas){
-	this.musicas = musicas || [];
-	}
+  constructor(musicas){
+    this.musicas = musicas || [];
+  }
 
-	 adicionarMusica(nome){
-	 let sons = this.musicas;
-		sons.push(nome);
-	}
+  adicionarMusica(nome){
+    this.musicas.push(nome);
+  }
 }
 
-let primeiraPlaylist = new Playlist(["Kuduru", "Anunciação", "Garçom"])
+function* generator(playlist){
+  for (const musica of playlist.musicas[Symbol.iterator]()){
+    yield musica;
+  }
+}
 
+function mostrarPlaylist(playlist,nome){
+  console.log("-".repeat(80));
+  for (const musica of generator(playlist)){
+    console.log(`Adicionada a música ${musica} para a Playlist -> (${nome})`);
+  }
+  console.log("-".repeat(80));
+}
+
+const primeiraPlaylist = new Playlist(["Kuduru", "Anunciação", "Garçom"]);
 primeiraPlaylist.adicionarMusica("Marvel Cinematic Song");
 
-for (let musica of primeiraPlaylist.musicas[Symbol.iterator]()){
-	console.log("Adicionada a música: " + musica);
-}
+const segundaPlaylist = new Playlist([]);
+segundaPlaylist.adicionarMusica("Blue Bird");
+segundaPlaylist.adicionarMusica("Forest Songs");
+segundaPlaylist.adicionarMusica("Drums Songs");
+
+mostrarPlaylist(primeiraPlaylist,"Eclético Ever");
+mostrarPlaylist(segundaPlaylist,"Sóbrio... por enquanto");
+
