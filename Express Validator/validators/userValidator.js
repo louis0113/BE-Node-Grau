cd46ecb3f8f923ec7c4b9ef64a390336e313d002
joaozinhoDoGrauxD <joaoninguem1301@gmail.com>
2025-10-13 18:12:08 -0300

    Remoção de gambiarras e readição de validações

--- Express Validator/validators/userValidator.js
+++ Express Validator/validators/userValidator.js
@@ -1,9 +1,12 @@
 const {body} = require("express-validator");
 
+const getUserWithIdValidation = [
+  body("id").isInt().isNumeric().not().isAlpha().withMessage("O param do ID deve ser um número inteiro")
+]
 const setUserValidation = [
   body("id").exists().isNumeric().isInt({min : 1}).not().isString().withMessage("Porfavor, coloque um inteiro maior que 1"),
   body("name").exists().isLength({min : 3}).withMessage("Coloque o nome com no mínimo 3 caracteres"),
   body("email").exists().isEmail().withMessage("Coloque um e-mail válido :)")
 ]
 
-module.exports = {setUserValidation};
+module.exports = {getUserWithIdValidation,setUserValidation};
