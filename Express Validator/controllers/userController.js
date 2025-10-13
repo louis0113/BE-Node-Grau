9f42153d7e2f09e018b43d1b9d4f176faab28d7e
louis0113 <luizhenrique011304@gmail.com>
2025-10-13 18:12:08 -0300

    Remoção de gambiarras e readição de validações

--- Express Validator/controllers/userController.js
+++ Express Validator/controllers/userController.js
@@ -1,53 +1,62 @@
 const {body, validationResult} = require("express-validator");
 
 let users = [];
-let x = 0;
-let getUserWithId = (req, res) => {
 
-  const id = req.params.id;
-
-  for (const user of users){
-    if( parseInt(user.id) === parseInt(id)){
-      res.json(user);
-    } else {
-      x++;
+function buscarDados(prop, func, param) {
+  for (const user of users) {
+    if (func(user[prop]) === func(param)) {
+      return user;
     }
   }
-
-  if (x != 0){
-    return res.status(404).json({"message" : `O id ${id} não existe`});
-    x = 0;
-  }
+  return null;
 };
 
-let getUserWithQuery = (req, res) => {
-
-  const name = req.query.name;
-  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-
-  if (url === "http://localhost:3000/api/users"){
-    res.json(users);
-  } else {
-    for (const user of users){
-      if (user.name  === name ){
-        res.json(user);
-      }  
-    }
-  }
-};
-
-let setUser = (req, res) => {
-
-  const newUser = req.body;
+const getUserWithId = (req, res) => {
 
   const errors = validationResult(req);
 
-  if (!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()})
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
   }
 
-  users.push(newUser)
-  res.status(201).json({"message" : "Usuário criado com sucesso!", "user" : newUser});
+  const userid = req.params.id;
+
+  const buscar = buscarDados("id", parseInt, userid);
+
+  if (!buscar) {
+    return res.status(404).json({"message": `O id ${userid} não existe`})
+  }
+
+  res.json(buscar);
+};
+
+const getUserWithQuery = (req, res) => {
+  const username = req.query.name;
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+  const expectedUrl = "http://localhost:3000/api/users";
+  if (url === expectedUrl || url === expectedUrl + "/") {
+    res.json(users);
+  } else {
+    const buscar = buscarDados("name", (val) => val.toLowerCase(), username.toLowerCase());
+
+    if (!buscar) {
+      return res.status(404).json({"message": `Usuário ${username} não encontrado`});
+    }
+
+    res.json(buscar);
+  }
+};
+
+const setUser = (req, res) => {
+  const newUser = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+
+  users.push(newUser);
+  res.status(201).json({"message": "Usuário criado com sucesso!", "user": newUser});
 };
 
 module.exports = {getUserWithId, getUserWithQuery, setUser};
