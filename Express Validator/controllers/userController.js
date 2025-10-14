fd0087462f1a25b61735b9cd23bc50fda92c2217
joaozinhoDoGrauxD <joaoninguem1301@gmail.com>
2025-10-14 09:58:43 -0300

    Reorganizando o código e traduzindo pro inglês

--- Express Validator/controllers/userController.js
+++ Express Validator/controllers/userController.js
@@ -1,62 +1,46 @@
-const {body, validationResult} = require("express-validator");
-
+const {validationResult} = require("express-validator");
 let users = [];
 
-function buscarDados(prop, func, param) {
+function searchData(prop, func, param) {
   for (const user of users) {
     if (func(user[prop]) === func(param)) {
       return user;
     }
   }
-  return null;
-};
+}
 
 const getUserWithId = (req, res) => {
-
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    return res.status(400).json({errors: errors.array()});
-  }
-
   const userid = req.params.id;
-
-  const buscar = buscarDados("id", parseInt, userid);
-
-  if (!buscar) {
-    return res.status(404).json({"message": `O id ${userid} não existe`})
+  const searchId = searchData("id", parseInt, userid);
+  if (!searchId) {
+    return res.status(404).json({"message": `The id ${userid} does not exist`})
   }
-
-  res.json(buscar);
+  res.json(searchId);
 };
 
 const getUserWithQuery = (req, res) => {
   const username = req.query.name;
-  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-  const expectedUrl = "http://localhost:3000/api/users";
-  if (url === expectedUrl || url === expectedUrl + "/") {
-    res.json(users);
-  } else {
-    const buscar = buscarDados("name", (val) => val.toLowerCase(), username.toLowerCase());
-
-    if (!buscar) {
-      return res.status(404).json({"message": `Usuário ${username} não encontrado`});
-    }
-
-    res.json(buscar);
+  if (!username) {
+    return res.json(users);
   }
+
+  const searchUser = searchData("name", (val) => val.toLowerCase(), username.toLowerCase());
+
+  if(!searchUser){
+    return res.status(404).json({"message" : `The user ${username} does not exist`})
+  }
+  res.json(searchUser);
 };
 
 const setUser = (req, res) => {
   const newUser = req.body;
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     return res.status(400).json({errors: errors.array()});
   }
-
   users.push(newUser);
-  res.status(201).json({"message": "Usuário criado com sucesso!", "user": newUser});
+  res.status(201).json({"message": "User created successfully!", "user": newUser});
 };
 
 module.exports = {getUserWithId, getUserWithQuery, setUser};
+
