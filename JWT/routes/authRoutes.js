8f153fa0e2a0af370c204bdb2cbfbe570e848da8
louis0113 <luizhenrique011304@gmail.com>
2025-10-26 18:04:43 -0300

    Refinamento de código e middleware de validação

--- JWT/routes/authRoutes.js
+++ JWT/routes/authRoutes.js
@@ -3,11 +3,17 @@ const express = require("express");
 const router = express.Router();
 const userValidations = require("../validators/authValidator");
 const authController = require("../controllers/authController");
-
-router.post("/login", userValidations.setLoginValidation, authController.login);
+const validateRequest = require("../middlewares/validateMiddleware");
+router.post(
+  "/login",
+  userValidations.setLoginValidation,
+  validateRequest,
+  authController.login,
+);
 router.post(
   "/register",
   userValidations.setRegisterValidation,
+  validateRequest,
   authController.register,
 );
 
