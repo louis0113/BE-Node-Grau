0ac371805abf9afdcea782ef149cefa3ecd9108f
joaozinhoDoGrauxD <joaoninguem1301@gmail.com>
2025-11-10 15:23:13 -0300

    fix: complete the tests of the exercise

--- TDD/test/config/database.js
+++ TDD/test/config/database.js
@@ -1,4 +1,23 @@
-let users = [];
+let users = [
+  {
+    id: 1,
+    username: "carlinhos",
+    email: "carlinhos@gmail.com",
+    password: "12345678",
+  },
+  {
+    id: 2,
+    username: "manueljose",
+    email: "manueljose@gmail.com",
+    password: "1",
+  },
+  {
+    id: 3,
+    username: "anaximenes",
+    email: "anaximenes@gmail.com",
+    password: "2",
+  },
+];
 
 let notas = [];
 
